15e1be9796321477cc92b3fd55a86ca733aa9723
cheng <18819206582@163.com>
2020-07-24 11:24:51 +0800

    适配图片的样式

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const config = {
   // api_base_url: "http://bl.7yue.pro/v1",
   // api_base_url: 'http://192.168.1.40:3000/v1',//本地
+  // api_base_url: 'http://192.168.1.40/v1',//本地
   // api_base_url: 'http://134.175.129.92:3000/v1',//本地(域名测试)
   api_base_url: 'https://www.ljcpro.cn/v1'
+  // api_base_url: "http://localhost:5000/v1"
   // appkey: "RdshydjBvcYZhMZC",
   // appkey: "AbhC31IG7ruCDp571"
 }
